a4f8170e1f6a166bdde5016286e9b86a1e376733
Thuhaa <josephthuha@gmail.com>
2022-01-25 00:42:37 +0300

    First Commit

---
+++ components/parameter-controls.js
@@ -0,0 +1,178 @@
+import { Box } from 'theme-ui'
+import { useCallback } from 'react'
+import { Slider, Badge, Toggle, Select, Link } from '@carbonplan/components'
+import { colormaps } from '@carbonplan/colormaps'
+
+const sx = {
+  label: {
+    fontFamily: 'mono',
+    letterSpacing: 'mono',
+    textTransform: 'uppercase',
+    fontSize: [1, 1, 1, 2],
+    mt: [3],
+  },
+}
+
+const CLIM_RANGES = {
+  KENYAZARR: { max: 1, min: 0},
+  lightscore: { max: 30, min: -20 },
+  prec: { max: 300, min: 0 },
+}
+
+const DEFAULT_COLORMAPS = {
+  lightscore: 'warm',
+  prec: 'cool',
+  KENYAZARR: 'warm',
+}
+
+const ParameterControls = ({ getters, setters }) => {
+  const { display, opacity, clim, month, band, colormapName } = getters
+  const {
+    setDisplay,
+    setOpacity,
+    setClim,
+    setMonth,
+    setBand,
+    setColormapName,
+  } = setters
+
+  const handleBandChange = useCallback((e) => {
+    const band = e.target.value
+    setBand(band)
+    setClim([CLIM_RANGES[band].min, CLIM_RANGES[band].max])
+    setColormapName(DEFAULT_COLORMAPS[band])
+  })
+
+  return (
+    <>
+      <Box sx={{ position: 'absolute', top: 20, right: 20 }}>
+        <Box sx={{ ...sx.label, mt: [0] }}>Display</Box>
+        <Toggle
+          sx={{ float: 'right', mt: [2] }}
+          value={display}
+          onClick={() => setDisplay((prev) => !prev)}
+        />
+      </Box>
+      <Box sx={{ position: 'absolute', top: 20, left: 20 }}>
+        <Box sx={{ ...sx.label, mt: [0] }}>Opacity</Box>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          sx={{ width: '175px', display: 'inline-block' }}
+          value={opacity}
+          onChange={(e) => setOpacity(parseFloat(e.target.value))}
+        />
+        <Badge
+          sx={{
+            bg: 'primary',
+            color: 'background',
+            display: 'inline-block',
+            fontSize: [1, 1, 1, 2],
+            height: ['21px', '21px', '21px', '23px'],
+            position: 'relative',
+            left: [3],
+            top: ['3px'],
+          }}
+        >
+          {opacity.toFixed(2)}
+        </Badge>
+        <Box sx={sx.label}>Minimum</Box>
+        <Slider
+          min={CLIM_RANGES[band].min}
+          max={CLIM_RANGES[band].max}
+          step={1}
+          sx={{ width: '175px', display: 'inline-block' }}
+          value={clim[0]}
+          onChange={(e) =>
+            setClim((prev) => [parseFloat(e.target.value), prev[1]])
+          }
+        />
+        <Badge
+          sx={{
+            bg: 'primary',
+            color: 'background',
+            display: 'inline-block',
+            position: 'relative',
+            left: [3],
+            top: [1],
+          }}
+        >
+          {clim[0].toFixed(0)}
+        </Badge>
+        <Box sx={sx.label}>Maximum</Box>
+        <Slider
+          min={CLIM_RANGES[band].min}
+          max={CLIM_RANGES[band].max}
+          step={1}
+          sx={{ width: '175px', display: 'inline-block' }}
+          value={clim[1]}
+          onChange={(e) =>
+            setClim((prev) => [prev[0], parseFloat(e.target.value)])
+          }
+        />
+        <Badge
+          sx={{
+            bg: 'primary',
+            color: 'background',
+            display: 'inline-block',
+            position: 'relative',
+            left: [3],
+            top: [1],
+          }}
+        >
+          {clim[1].toFixed(0)}
+        </Badge>
+        <Box sx={sx.label}>Month</Box>
+        <Slider
+          min={1}
+          max={12}
+          step={1}
+          sx={{ width: '175px', display: 'inline-block' }}
+          value={month}
+          onChange={(e) => setMonth(parseFloat(e.target.value))}
+        />
+        <Badge
+          sx={{
+            bg: 'primary',
+            color: 'background',
+            display: 'inline-block',
+            position: 'relative',
+            left: [3],
+            top: [1],
+          }}
+        >
+          {month.toFixed(0)}
+        </Badge>
+
+        <Box sx={{ ...sx.label, mt: [4] }}>Band</Box>
+        <Select
+          size='xs'
+          onChange={handleBandChange}
+          sx={{ mt: [1] }}
+          value={band}
+        >
+          <option value='KENYAZARR'>KENYAZARR</option>
+          <option value='prec'>Another Parameter</option>
+        </Select>
+
+        <Box sx={{ ...sx.label, mt: [4] }}>Colormap</Box>
+        <Select
+          size='xs'
+          onChange={(e) => setColormapName(e.target.value)}
+          sx={{ mt: [1] }}
+          value={colormapName}
+        >
+          {colormaps.map((d) => (
+            <option key={d.name}>{d.name}</option>
+          ))}
+        </Select>
+        {/*<Box sx={{ ...sx.label, mt: [4] }}>
+          <Link href='https://github.com/carbonplan/maps'>CODE ON Github</Link>
+          </Box>*/}
+      </Box>
+    </>
+  )
+}
+
+export default ParameterControls
---
+++ pages/index.js
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import { Box, useThemeUI } from 'theme-ui'
+import { Dimmer, Meta } from '@carbonplan/components'
+import { Map, Raster, Line, RegionPicker } from '@carbonplan/maps'
+import { useColormap } from '@carbonplan/colormaps'
+import RegionControls from '../components/region-controls'
+import ParameterControls from '../components/parameter-controls'
+
+const bucket = 'https://storage.googleapis.com/carbonplan-share/'
+
+const Index = () => {
+  const { theme } = useThemeUI()
+  const [display, setDisplay] = useState(true)
+  const [opacity, setOpacity] = useState(1)
+  const [clim, setClim] = useState([-20, 30])
+  const [month, setMonth] = useState(1)
+  const [band, setBand] = useState('lightscore')
+  const [colormapName, setColormapName] = useState('warm')
+  const colormap = useColormap(colormapName)
+  const [showRegionPicker, setShowRegionPicker] = useState(false)
+  const [regionData, setRegionData] = useState({ loading: true })
+
+  const getters = { display, opacity, clim, month, band, colormapName }
+  const setters = {
+    setDisplay,
+    setOpacity,
+    setClim,
+    setMonth,
+    setBand,
+    setColormapName,
+  }
+
+  return (
+    <>
+      <Meta
+        card={'https://images.carbonplan.org/social/maps-demo.png'}
+        description={
+          'UNDP Data - HREA'
+        }
+        title={'UNDP Data - HREA'}
+      />
+      <Box sx={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}>
+        <Map zoom={2} center={[0, 0]} debug={true}>
+          <Line
+            color={theme.rawColors.primary}
+            source={bucket + 'maps-demo/land'}
+            variable={'land'}
+          />
+          {showRegionPicker && (
+            <RegionPicker
+              color={theme.colors.primary}
+              backgroundColor={theme.colors.background}
+              fontFamily={theme.fonts.mono}
+              fontSize={'14px'}
+              maxRadius={2000}
+            />
+          )}
+          <Raster
+            colormap={colormap}
+            clim={clim}
+            display={display}
+            opacity={opacity}
+            mode={'texture'}
+            source={'http://localhost:8000'}
+            //source={bucket + 'maps-demo/4d/tavg-prec-month'}
+            //source={'https://undpngddlsgeohubdev01.blob.core.windows.net/test/kenyazarr/Data/a'}
+            variable={'lightscore'}
+            //selector={{ month, band }}
+            setRegionData={setRegionData}
+          />
+          <RegionControls
+            //band={band}
+            band={"lightscore"}
+            //month={month}
+            regionData={regionData}
+            showRegionPicker={showRegionPicker}
+            setShowRegionPicker={setShowRegionPicker}
+          />
+        </Map>
+        <ParameterControls getters={getters} setters={setters} />
+        <Dimmer
+          sx={{
+            display: ['initial', 'initial', 'initial', 'initial'],
+            position: 'absolute',
+            color: 'primary',
+            right: [13],
+            bottom: [17, 17, 15, 15],
+          }}
+        />
+      </Box>
+    </>
+  )
+}
+
+export default Index
